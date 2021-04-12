e729dddf8f4fb61d74b5947dc9cee243a0115530
Sayed Noman <Sayed.siddik@northsouth.edu>
2021-04-12 19:23:08 +0600

    course app created and added to settings

--- Project_Tutor_Hub/tutor_hub/urls.py
+++ Project_Tutor_Hub/tutor_hub/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 import home
-
+import courses
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+    path('',include('courses.urls')),
 ]
