e921d9cb6d7b2bfc629be926ae19bf31e0ea561a
Sayed Noman <Sayed.siddik@northsouth.edu>
2021-04-13 19:47:32 +0600

    coding standard followed, Class name altered

--- Project_Tutor_Hub/courses/admin.py
+++ Project_Tutor_Hub/courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from courses.models import Class,lecture
+from courses.models import Class,Lecture
 
 # Register your models here.
 admin.site.register(Class)
-admin.site.register(lecture)
+admin.site.register(Lecture)
--- Project_Tutor_Hub/courses/models.py
+++ Project_Tutor_Hub/courses/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-from home.models import tutor,student
+from home.models import Tutor,Student
 from django.db.models.signals import pre_save
 #from tutor_hub.utils import unique_slug_generator
 from courses.utils import unique_course_code_generator,unique_slug_generator
@@ -20,8 +20,8 @@ class Class(models.Model):
     slug = models.SlugField(null=True,blank=True)
     description = models.TextField(max_length=550,null=True,blank=True)
     image = models.ImageField(upload_to = save_class_image,blank=True,verbose_name ='class image')
-    tutor = models.ForeignKey(tutor, null=True, blank=True, on_delete=models.CASCADE,related_name='tutors')
-    students = models.ManyToManyField(student, blank=True)
+    tutor = models.ForeignKey(Tutor, null=True, blank=True, on_delete=models.CASCADE,related_name='tutors')
+    students = models.ManyToManyField(Student, blank=True)
 
     def __str__(self):
         return self.title
@@ -50,12 +50,12 @@ def save_lecture_files(instance,filename):
             filename = 'lecture_files/{}/{}/{}.{}'.format(instance.class_content.slug,instance.lecture_id, new_name,ext)
     return os.path.join(upload_to, filename)
 
-class lecture(models.Model):
+class Lecture(models.Model):
     lecture_id = models.CharField(max_length=100,null=True,blank=True)
     name = models.CharField(max_length=150)
     class_content= models.ForeignKey(Class, on_delete=models.CASCADE, related_name='lessons')
     description = models.TextField(max_length=550,null=True,blank=True)
-    created_by = models.ForeignKey(tutor,on_delete=models.CASCADE)
+    created_by = models.ForeignKey(Tutor,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     position = models.PositiveSmallIntegerField(verbose_name= 'Lecture No')
     slug = models.SlugField(null=True,blank=True)
