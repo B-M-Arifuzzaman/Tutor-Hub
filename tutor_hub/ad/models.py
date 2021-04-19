e0ddb4aeeee0dfee6b791f64fb9623bdce7d8e4c
Arif <bm.arifuzzaman@northsouth.edu>
2021-04-19 10:32:42 +0600

    Model for Tutor Advertise Added

--- tutor_hub/ad/models.py
+++ tutor_hub/ad/models.py
@@ -47,39 +47,39 @@ class Ad_Student(models.Model):
         '''
         return reverse('ad/home.html')
 
-# class Ad_Tutor(models.Model):
+class Ad_Tutor(models.Model):
     '''
     This class will create a table Ad_Tutor, where a tutor will fill the form to request for a tutor.
     In that table the fields will be the variables, fields attributes are also defined here. 
     '''
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=150, null=True, blank=True)
-#     expected_area = models.CharField(max_length=150, null=True, blank=True)
-#     subject = models.CharField(max_length=150, null=True, blank=True)
-#     class_level = models.CharField(max_length=150, null=True, blank=True)
-#     days = models.DecimalField(decimal_places=0, max_digits=7)
-#     expected_salary = models.DecimalField(decimal_places=1000, max_digits=50000)
-#     male ='male'
-#     female = 'female'
-#     other = 'other'
-#     genders = [
-#         (male,'male'),
-#         (female,'female'),
-#         (other,'other'),
-#     ]
-#     gender = models.CharField(max_length=15, choices=genders,default=male,null=True,blank=True)
-#     description = models.TextField(null=True, blank=True)
-#     request_confirmation = models.BooleanField(default=False)
-#     ad_created = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, null=True, blank=True)
+    expected_area = models.CharField(max_length=150, null=True, blank=True)
+    subject = models.CharField(max_length=150, null=True, blank=True)
+    class_level = models.CharField(max_length=150, null=True, blank=True)
+    days = models.DecimalField(decimal_places=0, max_digits=7)
+    expected_salary = models.DecimalField(decimal_places=1000, max_digits=50000)
+    male ='male'
+    female = 'female'
+    other = 'other'
+    genders = [
+        (male,'male'),
+        (female,'female'),
+        (other,'other'),
+    ]
+    gender = models.CharField(max_length=15, choices=genders,default=male,null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
+    request_confirmation = models.BooleanField(default=False)
+    ad_created = models.DateTimeField(auto_now_add=True)
     
     
-#     def __str__(self):
+    def __str__(self):
             '''
             This will take the self value of this function. 
             :type self: string
             :param self: Takes the object representation in string format.
             '''
-#         return self.title
+        return self.title
     
     
     
