1c69345f7d14a5c97e4998b30f1cf07392b2d785
Sayed Noman <Sayed.siddik@northsouth.edu>
2021-04-21 14:29:46 +0600

    documentation for forms.py done

--- Project_Tutor_Hub/courses/forms.py
+++ Project_Tutor_Hub/courses/forms.py
@@ -7,6 +7,12 @@ from courses.models import Class,Lecture
 
 
 class CreateClassForm(ModelForm):
+    """
+    This is a conceptual class representation of Create Class Form for tutor to be used in template.
+    :param ModelForm: It inherits built-in functionalities of django ModelForm, which handels all validations
+    :type ModelForm: ModelForm
+
+    """
     title = forms.CharField(label='Course Name', max_length=100)
     description = forms.CharField(widget=forms.Textarea(attrs={'rows':3}))
 
@@ -15,6 +21,12 @@ class CreateClassForm(ModelForm):
         fields = ['title', 'description']
 
 class CreateLectureForm(forms.ModelForm):
+    """
+    This is a conceptual class representation of Create lecture Form for tutor to be used in template.
+    :param ModelForm: It inherits built-in functionalities of django `forms.ModelForm`, which handels all validations
+    :type ModelForm: `forms.ModelForm`
+    
+    """
     class Meta:
         model = Lecture
         fields = ('name','position','description','video','ppt','notes')
