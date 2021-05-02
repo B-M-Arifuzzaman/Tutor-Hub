5ddb09d75b2c8dd1c39b26043b6ffb5fa839891c
kingkhanthekk <abul.kalam10@northsouth.edu>
2021-05-02 22:39:31 +0600

    modified urls.py to include details app urls

--- Project_Tutor_Hub/tutor_hub/urls.py
+++ Project_Tutor_Hub/tutor_hub/urls.py
@@ -19,6 +19,7 @@ from details import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('details.urls')),
     path('view_more/<int:pk>/',
          views.view_more, name='view_more'),
     path('', views.home, name='home'),
