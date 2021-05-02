5e201c9d7e8e3ecbcf298537e465e7a5b5d27d55
B. M. Arifuzzaman <bm.arifuzzaman@northsouth.edu>
2021-05-02 12:24:00 +0600

    apps.py & forms.py commented properly

--- tutor_hub/ad/apps.py
+++ tutor_hub/ad/apps.py
@@ -1,5 +1,13 @@
+'''
+This program will create a visual database in Admin panel.
+'''
 from django.apps import AppConfig
 
 
 class AdConfig(AppConfig):
+    '''
+    This is a Visual Database representation of all apps in Admin panel.
+    :param AppConfig: It configures the database in Admin panel.
+    :type AppConfig: string
+    '''
     name = 'ad'
--- tutor_hub/ad/forms.py
+++ tutor_hub/ad/forms.py
@@ -11,7 +11,7 @@ class AdStudentForm(ModelForm):
     '''
     This is a conceptual Form representation of Class table for student ads.
     :param ModelForm: It creates built-in html form of django, which handels all validations in django Admin panel.
-    :type ModelForm: html form 
+    :type ModelForm: model, fields
     '''
     class Meta:
         model = AdStudent
@@ -21,7 +21,7 @@ class AdTutorForm(ModelForm):
     '''
     This is a conceptual Form representation of Class table for tutor ads.
     :param ModelForm: It creates built-in html form of django, which handels all validations in django Admin panel.
-    :type ModelForm: html form 
+    :type ModelForm: model, fields
     '''
     class Meta:
         model = AdTutor
