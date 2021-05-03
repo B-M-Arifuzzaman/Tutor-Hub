de2caa7528bc45519b21ac9c55f39107ad31d4c2
Faria <mir.faria@northsouth.edu>
2021-05-03 22:51:56 +0600

    Test of Urls is modified

--- Project_Tutor_Hub/home/tests/test_urls.py
+++ Project_Tutor_Hub/home/tests/test_urls.py
@@ -7,17 +7,17 @@ class TestUrls(SimpleTestCase):
         url= reverse('profile')
         self.assertEqual(resolve(url).func, profile)
 
-class TestUrls(SimpleTestCase):
+
     def test_profile_url_is_resolved(self):
         url= reverse('edit_profile_tutor')
         self.assertEqual(resolve(url).func, edit_profile_tutor)
 
-class TestUrls(SimpleTestCase):
+
     def test_profile_url_is_resolved(self):
         url= reverse('edit_profile_student')
         self.assertEqual(resolve(url).func, edit_profile_student)
 
-class TestUrls(SimpleTestCase):
+
     def test_profile_url_is_resolved(self):
         url= reverse('delete_profile')
         self.assertEqual(resolve(url).func, delete_profile)
