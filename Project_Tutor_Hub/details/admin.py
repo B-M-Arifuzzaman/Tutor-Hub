7f36b7d974a237c5252bea9aac9855d9d73d4bfd
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2021-05-06 20:35:18 +0600

    Admin.py modified as no model used

--- Project_Tutor_Hub/details/admin.py
+++ Project_Tutor_Hub/details/admin.py
@@ -1,9 +1,7 @@
+
 '''
 This program will create visual database pages in Admin panel.
 '''
 from django.contrib import admin
-from .models import Ad_Student, Ad_Tutor
 
-
-admin.site.register(Ad_Student)
-admin.site.register(Ad_Tutor)
+# Register your models here.
